a57e2223540cf526b574678c93a3671bda1d48ef
Buse Şentürk <buse.senturk@outlook.com>
2021-05-04 14:08:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ let num1 = 2;
 let num2 = 31;
 let multiply = num1 * num2;
 // Write your code here
-
-
 let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 14;
+let num4 = 10;
+let mod = num3 % num4;
+
